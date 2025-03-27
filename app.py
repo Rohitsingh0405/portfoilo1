0dbdefd776c59178de0d44a0b93da804daa17d85
Rohit Singh <rs21Rohit@gmail.com>
2025-03-27 22:39:15 +0530

    .

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import json
+import xmltodict
 # import sys
 # sys.path.append('./pages/Home.py')
 # import pages
 # import pages.Home as b
 
+import pages.About
 import pages.Home
 
 # from pages import home
@@ -18,4 +21,25 @@ col1,col2,col3 = st.columns([1,0.3,1])
 if selected_menu=="Home":
      # Home.pages
      pages.home()
+     with open("Day 3.pdf",'rb'):
+          st.download_button("donwload","Day 3.pdf")
+          a = st.file_uploader("Add text file !")
+          if a:
+               for line in a:
+                    
+                    st.write(line)
+                    print(line)
+     
+          # xml = a.read()
+          # file1_data = json.loads(json.dumps(xmltodict.parse(xml)))
+          # st.write(file1_data)
+#           uploaded_files = st.file_uploader(
+#     "Choose a CSV file", accept_multiple_files=True
+# )
+# for uploaded_file in uploaded_files:
+#     bytes_data = uploaded_file.read()
+#     st.write("filename:", uploaded_file.name)
+#     st.write(bytes_data)
 
+# if selected_menu=='About':
+#      pages.About()
